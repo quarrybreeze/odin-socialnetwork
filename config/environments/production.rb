2401406486bd0d76295d53e6e0fec5c9723eb602
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-04-14 21:29:46 -0700

    update: another attempt at fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,9 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # 
+  
+  config.action_mailer.delivery_method = :smtp # or whatever you're using
+  config.action_mailer.perform_deliveries = false # set to true when ready
+
 end
