a06507b412502575ddcb660ed9c26834aee46dcf
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-04-14 20:20:44 -0700

    NEW: added mailer feature in development environment using letter_opener gem

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ group :test do
 end
 
 gem "devise", "~> 4.9"
+gem "letter_opener", group: :development
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :send_welcome_email
+
   has_many :authored_posts, class_name: "Post", foreign_key: "author_id"
   has_many :likes, dependent: :destroy
 
@@ -12,4 +14,10 @@ class User < ApplicationRecord
 
   has_many :following_user, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
   has_many :following, through: :following_user, source: :followed_user
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_later
+  end
 end
